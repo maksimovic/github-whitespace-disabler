415d42cc65d46afd17a696165b959ab17b0e2d15
Oliver Maksimovic <oliver@maksimovic.net>
2017-07-09 23:39:52 +0200

    WIP: local storage settings for stripping the whitespace

--- interceptor.js
+++ interceptor.js
@@ -1,24 +1,55 @@
+const on = 'On';
+const off = 'Off';
+
+var default_settings = {
+    'strip_whitespace' : on
+};
+
+function on_error(e) {
+    console.error(e);
+}
+
+const get_stored_settings = browser.storage.local.get();
+get_stored_settings.then(function(settings) {
+    if (typeof settings.strip_whitespace === 'undefined') {
+        browser.storage.local.set(default_settings);
+    }
+
+    console.log(settings);
+}, on_error);
+
 /**
  * @param {object} requestDetails
+ * @param {object} settings
  */
-function redirect(requestDetails) {
+function redirect(requestDetails, settings) {
     var url = new URL(requestDetails.url);
 
-    if (!url.searchParams.has('w')) {
+    console.log(settings);
+    console.log(requestDetails);
+
+    if (!url.searchParams.has('w') && settings.strip_whitespace === on) {
         url.searchParams.set('w', 1);
     }
+
+    console.log(url.href);
 
     return {
         redirectUrl : url.href
     };
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-    redirect,
-    { urls : [
-        "*://github.com/*/commit/*",
-        "*://github.com/*/*/pull/*/files"
-    ]
+browser.webRequest.onBeforeRequest.addListener(function(requestDetails) {
+        var get_settings = browser.storage.local.get();
+        get_settings.then(function(settings) {
+            return redirect(requestDetails, settings);
+        });
     },
-    ["blocking"]
+    {
+        urls : [
+            "*://github.com/*/commit/*",
+            "*://github.com/*/*/pull/*/files"
+        ]
+    },
+    [ "blocking" ]
 );
